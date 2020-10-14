13738e79f21c86d1689610b64c5fa95267572df4
Adam Hałdaś <adam.haldas@gmail.com>
2020-10-14 13:15:27 +0200

    Updates

---
+++ precache-manifest.4013d7368053d3ae5da3d9918caf16ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e200b1dcb210a875a63772d3150f8219",
+    "url": "/reactnavbar/index.html"
+  },
+  {
+    "revision": "7e1858ae3854fa49308e",
+    "url": "/reactnavbar/static/css/main.75b94caa.chunk.css"
+  },
+  {
+    "revision": "15d704e1ba387b60bfe2",
+    "url": "/reactnavbar/static/js/2.1da5956b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactnavbar/static/js/2.1da5956b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e1858ae3854fa49308e",
+    "url": "/reactnavbar/static/js/main.de83a081.chunk.js"
+  },
+  {
+    "revision": "7e7140002f62c0d7aa18",
+    "url": "/reactnavbar/static/js/runtime-main.91c840b9.js"
+  }
+]);

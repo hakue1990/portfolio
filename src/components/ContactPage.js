a6201763c9294a77a377efe784198e14852b51e7
Adam Hałdaś <adam.haldas@gmail.com>
2020-10-14 13:14:52 +0200

    add gh-pages

---
+++ src/components/ContactPage.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const ContactPage = () => {
+  return (
+    <>
+      <main>
+        <p>
+          <h1>Contact Me</h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+          sit aliquam provident quod fuga perferendis iste ex ipsa eum atque
+          minus asperiores error, cumque ducimus autem vero eos quas corporis
+          laboriosam dolor quam quibusdam esse distinctio. Adipisci ducimus
+          ipsum laudantium voluptatum consequatur fugit maiores saepe esse minus
+          asperiores sit quidem quis magni error deleniti quos incidunt modi,
+          unde deserunt cumque, praesentium quae nesciunt. Sit aliquid dolorem,
+          earum similique vero alias mollitia. Libero excepturi animi
+          dignissimos, laboriosam harum nihil earum mollitia. Veritatis commodi,
+          optio ratione quo consectetur, deserunt aperiam reprehenderit ad
+          labore quis accusantium soluta obcaecati. Asperiores eaque dicta
+          perferendis consequatur, nisi, veritatis itaque blanditiis, voluptatum
+          ipsum a numquam et deleniti! Ratione quos illo aliquam pariatur eius
+          odio tempora possimus neque. Quibusdam, corporis fugit deserunt odit
+          pariatur nulla, velit quisquam ea praesentium enim explicabo iure
+          omnis. Suscipit numquam nemo molestias cum fugit pariatur commodi
+          veniam, esse repellat debitis aspernatur? Quaerat, ab. s sit quidem
+          quis magni error deleniti quos incidunt modi, unde deserunt cumque,
+          praesentium quae nesciunt. Sit aliquid dolorem, earum similique vero
+          alias mollitia. Libero excepturi animi s sit quidem quis magni error
+          deleniti quos incidunt modi, unde deserunt cumque, praesentium quae
+          nesciunt. Sit aliquid dolorem, earum similique vero alias mollitia.
+          Libero excepturi animi s sit quidem quis magni error deleniti quos
+          incidunt modi, unde deserunt cumque, praesentium quae nesciunt. Sit
+          aliquid dolorem, earum similique vero alias mollitia. Libero excepturi
+          animi
+        </p>
+      </main>
+    </>
+  );
+};
+
+export default ContactPage;
